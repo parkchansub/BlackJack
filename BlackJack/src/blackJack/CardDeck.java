0e922330baf8a4dd4598fbc1443859592f41b1d7
cs.park <pcs0331@naver.com>
2019-03-28 11:24:49 +0900

    테스트

--- BlackJack/src/blackJack/CardDeck.java
+++ BlackJack/src/blackJack/CardDeck.java
@@ -3,7 +3,7 @@ package blackJack;
 import java.util.List;
 
 public class CardDeck {
-    private List<Card> cards;
+    private List<Card> cards; 
 
     public Card getCard(){
         return null;
