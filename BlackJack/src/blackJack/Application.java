70493501c68667884fe5653ebc5ce400deb8193a
cs.park <pcs0331@naver.com>
2019-03-29 15:01:26 +0900

    항목 추가

---
+++ BlackJack/src/blackJack/Application.java
@@ -0,0 +1,10 @@
+package blackJack;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.play();
+	}
+
+}
---
+++ BlackJack/src/blackJack/Rule.java
@@ -0,0 +1,10 @@
+package blackJack;
+
+import java.util.List;
+
+public class Rule {
+	public int getScore(List<Card> cards){
+		return 0;
+	}
+	public void getWinner(Dealer dealer, Gamer gamer){}
+}
